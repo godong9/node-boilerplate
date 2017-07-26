e51a1a73efd1be429749768ad45de895c70af459
gd.godong9 <gd.godong9@kakaocorp.com>
2017-07-26 16:31:17 +0900

    Init project setting

---
+++ app/public/test.js
@@ -0,0 +1,4 @@
+const RED = Symbol("The color of a sunset!");
+const ORANGE = Symbol("The color of a sunset!");
+
+console.log(RED === ORANGE);
---
+++ gulpfile.babel.js
@@ -0,0 +1,45 @@
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+import eslint from 'gulp-eslint';
+import rimraf from 'rimraf';
+import sourcemaps from 'gulp-sourcemaps';
+import nodemon from 'gulp-nodemon';
+import mocha from 'gulp-mocha';
+
+const SRC_FILES = 'app/**/*.js';
+const SRC_PATH = 'app';
+const DIST_PATH = 'dist';
+
+gulp.task('clean', () => rimraf.sync(DIST_PATH));
+
+gulp.task('build', ['clean'], () => gulp.src(SRC_FILES)
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(DIST_PATH))
+);
+
+gulp.task('test', () => {
+    gulp.src([SRC_FILES])
+        .pipe(mocha({reporter: 'nyan'}))
+});
+
+gulp.task('server', ['build'], () => {
+    return nodemon({
+        script: `./${DIST_PATH}/app.js`,
+        watch: [ SRC_PATH ],
+        tasks: [ 'build' ],
+        env: { NODE_ENV: 'development' }
+    });
+});
+
+gulp.task('default', function() {
+    // ESLint 실행
+    gulp.src([SRC_FILES])
+        .pipe(eslint())
+        .pipe(eslint.format());
+    // Babel 실행
+    gulp.src(SRC_FILES)
+        .pipe(babel())
+        .pipe(gulp.dest(DIST_PATH));
+});
