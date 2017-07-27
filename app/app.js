3c85e91667b2e89c362f6be760a9f060bbb3e265
gd.godong9 <gd.godong9@kakaocorp.com>
2017-07-28 08:54:11 +0900

    Add body-parser, cookie-parser

--- app/app.js
+++ app/app.js
@@ -4,9 +4,12 @@ import config from 'config';
 import log4js from 'log4js';
 import path from 'path';
 import index from './routes/index';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+
+const MAXIMUM_BODY_SIZE = '10mb';
 
 log4js.configure(config.get('log4js'));
-
 const logger = log4js.getLogger('app');
 const app = express();
 const port = process.env.PORT || config.port;
@@ -14,6 +17,15 @@ const port = process.env.PORT || config.port;
 logger.info(`env.NODE_ENV: ${process.env.NODE_ENV}`);
 
 app.use(morgan('combined'));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ limit: MAXIMUM_BODY_SIZE, extended: false }));
+
+// parse application/json
+app.use(bodyParser.json({ limit: MAXIMUM_BODY_SIZE }));
+
+// cookie parse
+app.use(cookieParser());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
