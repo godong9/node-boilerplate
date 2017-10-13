a9f03bfe569106076ec15ee4cb7fd913d136985e
donghyun-go <donghyun.go@navercorp.com>
2017-10-13 14:12:29 +0900

    spec 작성시 #메서드 형태의 describe 추가

--- test/services/contents.spec.js
+++ test/services/contents.spec.js
@@ -4,99 +4,104 @@ import UserService from "../../app/services/users";
 import ContentService from "../../app/services/contents";
 
 describe("ContentService.getContents", () => {
-  before((done) => {
-    // given
-    ContentService.deleteAll()
-      .then(() => UserService.deleteAll())
-      .then(() => UserService.saveUser({ nickname: "test", email: "test@test.com", }))
-      .then((user) => ContentService.saveContent({ title: "test title", text: "test text", userId: user.id, }))
-      .then(() => {
-        done();
-      });
-  });
+  describe("#getContents", () => {
+    before((done) => {
+      // given
+      ContentService.deleteAll()
+        .then(() => UserService.deleteAll())
+        .then(() => UserService.saveUser({ nickname: "test", email: "test@test.com", }))
+        .then((user) => ContentService.saveContent({ title: "test title", text: "test text", userId: user.id, }))
+        .then(() => {
+          done();
+        });
+    });
 
-  it("should get contents", (done) => {
-    // when
-    ContentService.getContents()
-      .then(contents => {
-        // then
-        expect(contents.length).to.equal(1);
-        expect(contents[0].title).to.equal("test title");
-        expect(contents[0].text).to.equal("test text");
-        expect(contents[0].user.nickname).to.equal("test");
-        expect(contents[0].user.email).to.equal("test@test.com");
-        done();
-      });
-  });
-});
-
-describe("ContentService.getContentsByUserId", () => {
-  let testUser = null;
-
-  before((done) => {
-    // given
-    ContentService.deleteAll()
-      .then(() => UserService.deleteAll())
-      .then(() => UserService.saveUser({ nickname: "test", email: "test@test.com", }))
-      .then((user) => {
-        testUser = user;
-        return ContentService.saveContent({ title: "test title", text: "test text", userId: user.id, });
-      })
-      .then(() => {
-        done();
-      });
-  });
-
-  it("should get null", (done) => {
-    // when
-    ContentService.getContentsByUserId(-1)
-      .then(contents => {
-        // then
-        expect(contents.length).to.equal(0);
-        done();
-      });
-  });
-
-  it("should get contents by user id", (done) => {
-    // when
-    ContentService.getContentsByUserId(testUser.id)
-      .then(contents => {
-        // then
-        expect(contents.length).to.equal(1);
-        expect(contents[0].title).to.equal("test title");
-        expect(contents[0].text).to.equal("test text");
-        expect(contents[0].user.nickname).to.equal("test");
-        expect(contents[0].user.email).to.equal("test@test.com");
-        done();
-      });
+    it("should get contents", (done) => {
+      // when
+      ContentService.getContents()
+        .then(contents => {
+          // then
+          expect(contents.length).to.equal(1);
+          expect(contents[0].title).to.equal("test title");
+          expect(contents[0].text).to.equal("test text");
+          expect(contents[0].user.nickname).to.equal("test");
+          expect(contents[0].user.email).to.equal("test@test.com");
+          done();
+        });
+    });
   });
 });
 
-describe("ContentService.getContent", () => {
-  let testContent = null;
+describe("ContentService", () => {
+  describe("#getContentsByUserId", () => {
+    let testUser = null;
+    before((done) => {
+      // given
+      ContentService.deleteAll()
+        .then(() => UserService.deleteAll())
+        .then(() => UserService.saveUser({ nickname: "test", email: "test@test.com", }))
+        .then((user) => {
+          testUser = user;
+          return ContentService.saveContent({ title: "test title", text: "test text", userId: user.id, });
+        })
+        .then(() => {
+          done();
+        });
+    });
 
-  before((done) => {
-    // given
-    ContentService.deleteAll()
-      .then(() => UserService.deleteAll())
-      .then(() => UserService.saveUser({ nickname: "test", email: "test@test.com", }))
-      .then((user) => ContentService.saveContent({ title: "test title", text: "test text", userId: user.id, }))
-      .then((content) => {
-        testContent = content;
-        done();
-      });
+    it("should get null", (done) => {
+      // when
+      ContentService.getContentsByUserId(-1)
+        .then(contents => {
+          // then
+          expect(contents.length).to.equal(0);
+          done();
+        });
+    });
+
+    it("should get contents by user id", (done) => {
+      // when
+      ContentService.getContentsByUserId(testUser.id)
+        .then(contents => {
+          // then
+          expect(contents.length).to.equal(1);
+          expect(contents[0].title).to.equal("test title");
+          expect(contents[0].text).to.equal("test text");
+          expect(contents[0].user.nickname).to.equal("test");
+          expect(contents[0].user.email).to.equal("test@test.com");
+          done();
+        });
+    });
   });
+});
 
-  it("should get content", (done) => {
-    // when
-    ContentService.getContent(testContent.id)
-      .then(content => {
-        // then
-        expect(content.title).to.equal("test title");
-        expect(content.text).to.equal("test text");
-        expect(content.user.nickname).to.equal("test");
-        expect(content.user.email).to.equal("test@test.com");
-        done();
-      });
+describe("ContentService", () => {
+  describe("#getContent", () => {
+    let testContent = null;
+
+    before((done) => {
+      // given
+      ContentService.deleteAll()
+        .then(() => UserService.deleteAll())
+        .then(() => UserService.saveUser({ nickname: "test", email: "test@test.com", }))
+        .then((user) => ContentService.saveContent({ title: "test title", text: "test text", userId: user.id, }))
+        .then((content) => {
+          testContent = content;
+          done();
+        });
+    });
+
+    it("should get content", (done) => {
+      // when
+      ContentService.getContent(testContent.id)
+        .then(content => {
+          // then
+          expect(content.title).to.equal("test title");
+          expect(content.text).to.equal("test text");
+          expect(content.user.nickname).to.equal("test");
+          expect(content.user.email).to.equal("test@test.com");
+          done();
+        });
+    });
   });
 });
--- test/services/users.spec.js
+++ test/services/users.spec.js
@@ -2,91 +2,95 @@ import { expect } from "chai";
 
 import UserService from "../../app/services/users";
 
-describe("UserService.getUsers", () => {
-  before((done) => {
-    // given
-    UserService.deleteAll()
-      .then(() => UserService.saveUser({ nickname: "test1", email: "test1@test.com", }))
-      .then(() => UserService.saveUser({ nickname: "test2", email: "test2@test.com", }))
-      .then(() => done());
-  });
+describe("UserService", () => {
+  describe("#getUsers", () => {
+    before((done) => {
+      // given
+      UserService.deleteAll()
+        .then(() => UserService.saveUser({ nickname: "test1", email: "test1@test.com", }))
+        .then(() => UserService.saveUser({ nickname: "test2", email: "test2@test.com", }))
+        .then(() => done());
+    });
 
-  it("should get all users", (done) => {
-    // when
-    const params = {
-      order:
-      [
-        [
-          "nickname", "DESC",
-        ],
-      ],
-    };
-    UserService.getUsers(params)
-      .then(users => {
-        // then
-        expect(users.length).to.equal(2);
-        done();
-      });
-  });
-
-  it("should get test1 user", (done) => {
-    // when
-    const params = { where: { nickname: "test1", }, };
-    UserService.getUsers(params)
-      .then(users => {
-        // then
-        expect(users.length).to.equal(1);
-        expect(users[0].nickname).to.equal("test1");
-        expect(users[0].email).to.equal("test1@test.com");
-        done();
-      });
-  });
-
-  it("should get all users order by nickname desc", (done) => {
-    // when
-    const params = {
-      order:
+    it("should get all users", (done) => {
+      // when
+      const params = {
+        order:
         [
           [
             "nickname", "DESC",
           ],
         ],
-    };
-    UserService.getUsers(params)
-      .then(users => {
-        // then
-        expect(users.length).to.equal(2);
-        expect(users[0].nickname).to.equal("test2");
-        expect(users[0].email).to.equal("test2@test.com");
-        expect(users[1].nickname).to.equal("test1");
-        expect(users[1].email).to.equal("test1@test.com");
-        done();
-      });
+      };
+      UserService.getUsers(params)
+        .then(users => {
+          // then
+          expect(users.length).to.equal(2);
+          done();
+        });
+    });
+
+    it("should get test1 user", (done) => {
+      // when
+      const params = { where: { nickname: "test1", }, };
+      UserService.getUsers(params)
+        .then(users => {
+          // then
+          expect(users.length).to.equal(1);
+          expect(users[0].nickname).to.equal("test1");
+          expect(users[0].email).to.equal("test1@test.com");
+          done();
+        });
+    });
+
+    it("should get all users order by nickname desc", (done) => {
+      // when
+      const params = {
+        order:
+          [
+            [
+              "nickname", "DESC",
+            ],
+          ],
+      };
+      UserService.getUsers(params)
+        .then(users => {
+          // then
+          expect(users.length).to.equal(2);
+          expect(users[0].nickname).to.equal("test2");
+          expect(users[0].email).to.equal("test2@test.com");
+          expect(users[1].nickname).to.equal("test1");
+          expect(users[1].email).to.equal("test1@test.com");
+          done();
+        });
+    });
   });
 });
 
-describe("UserService.getUser", () => {
-  let testUser = null;
+describe("UserService", () => {
+  describe("#getUser", () => {
+    let testUser = null;
 
-  before((done) => {
-    // given
-    UserService.deleteAll()
-      .then(() => UserService.saveUser({ nickname: "test", email: "test@test.com", }))
-      .then(function (user) {
-        testUser = user;
-        done();
-      });
-  });
+    before((done) => {
+      // given
+      UserService.deleteAll()
+        .then(() => UserService.saveUser({ nickname: "test", email: "test@test.com", }))
+        .then(function (user) {
+          testUser = user;
+          done();
+        });
+    });
 
-  it("should get user", (done) => {
-    // when
-    UserService.getUser(testUser.id)
-      .then(user => {
-        // then
-        expect(user.nickname).to.equal("test");
-        expect(user.email).to.equal("test@test.com");
-        done();
-      });
+    it("should get user", (done) => {
+      // when
+      UserService.getUser(testUser.id)
+        .then(user => {
+          // then
+          expect(user.nickname).to.equal("test");
+          expect(user.email).to.equal("test@test.com");
+          done();
+        });
+    });
   });
 });
 
